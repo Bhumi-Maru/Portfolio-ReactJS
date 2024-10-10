6a98a278d8d0a5071f6f03cde595c61ef5baf5c1
Bhumi-Maru <bhumimaru7@gmail.com>
2024-10-10 17:02:42 +0530

    constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges With a passion for learning. I am dedicated to delivering high-quality results With a positive attitude and a growth mindset. I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Bhumi-Maru",
   resume:
-    "https://drive.google.com/file/d/1L9Ylh4hIMqL4uyn0Fg1SpdHlmCyzEj3i/view?usp=sharing",
+    "https://drive.google.com/file/d/1L9Ylh4hIMqL4uyn0Fg1SpdHlmCyzEj3i/view",
   linkedin: "https://www.linkedin.com/in/bhumi-maru/",
   github: "https://github.com/Bhumi-Maru",
 };
