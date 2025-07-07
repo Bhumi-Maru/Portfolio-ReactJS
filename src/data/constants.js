f62269b5e0301c1321d54aba0045f4e60f5c15b7
Bhumi-Maru <bhumimaru7@gmail.com>
2025-07-07 14:57:41 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -130,114 +130,114 @@ export const projects = [
     title: "Eduford",
     description:
       "This HTML and CSS code creates a responsive university website with multiple sections, including a navigation bar, a header featuring a banner image and text, course offerings, global campus locations, facilities, testimonials, a call-to-action for course enrollment, and a footer. The site includes interactive elements such as a toggle menu for smaller screens, hover effects on course and campus items, and star ratings for testimonials. The design is visually enhanced with background images, icons, and a color scheme of red and white. The layout is optimized for both desktop and mobile devices.",
-    image: "https://i.imghippo.com/files/LEAfd1728454227.png",
+    image: "https://i.imghippo.com/files/Sbj7964Bbs.png",
     tags: ["HTML", "CSS"],
     github: "https://github.com/Bhumi-Maru/Eduford",
     webapp: "https://eduford-by-maru.netlify.app/",
   },
   ,
+  // {
+  //   id: 1,
+  //   title: "Bootstrap Portfolio",
+  //   description:
+  //     "This portfolio website is a sleek and modern platform designed to showcase personal or professional work. Built with HTML, CSS, Bootstrap, and JavaScript, it features a responsive layout that adapts seamlessly to different devices. The homepage includes a visually appealing hero section with smooth scrolling navigation. Bootstrap's grid system is used to organize project galleries, skillsets, and testimonials in a clean and structured way. Interactive elements, such as hover effects and animations, enhance the user experience.",
+  //   image: "https://i.imghippo.com/files/KtKgC1726730496.png",
+  //   tags: ["HTML", "CSS", "BootStrap"],
+  //   github: "https://github.com/Bhumi-Maru/Portfolio",
+  //   webapp: "https://bootstrap-portfolio-by-maru.netlify.app/",
+  // },
   {
     id: 1,
-    title: "Bootstrap Portfolio",
-    description:
-      "This portfolio website is a sleek and modern platform designed to showcase personal or professional work. Built with HTML, CSS, Bootstrap, and JavaScript, it features a responsive layout that adapts seamlessly to different devices. The homepage includes a visually appealing hero section with smooth scrolling navigation. Bootstrap's grid system is used to organize project galleries, skillsets, and testimonials in a clean and structured way. Interactive elements, such as hover effects and animations, enhance the user experience.",
-    image: "https://i.imghippo.com/files/KtKgC1726730496.png",
-    tags: ["HTML", "CSS", "BootStrap"],
-    github: "https://github.com/Bhumi-Maru/Portfolio",
-    webapp: "https://bootstrap-portfolio-by-maru.netlify.app/",
-  },
-  {
-    id: 2,
     title: "Cactus plant website",
     description:
       "The HTML, CSS, and JavaScript on this cactus plant website work together to create an interactive and visually appealing experience. The HTML provides the structure and content of the site, including sections for home, news, shop, care, and contact, along with a navigation menu and footer. The CSS styles these elements, ensuring a responsive design with a clean layout and modern look. It includes features like a blur effect on the header when scrolling and visibility toggling for the scroll-up button. JavaScript enhances functionality by enabling the navigation menu to toggle open and closed, hiding the menu when links are clicked, applying a blur effect to the header on scroll, displaying the scroll-up button based on scroll position, highlighting active sections in the navigation, and implementing smooth scroll reveal animations for a polished and engaging user experience.",
-    image: "https://i.imghippo.com/files/t5oJ81726739378.png",
+    image: "https://i.imghippo.com/files/Bbx7911SmA.png",
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/Bhumi-Maru/Cactus-Website",
     webapp: "https://cactus-website-by-maru.netlify.app/",
   },
   {
-    id: 3,
+    id: 2,
     title: "Count-Down App",
     description:
       "This HTML and JavaScript code creates a countdown timer that shows the remaining time until a specific future date, 20 September 2024, 19:00:00. The HTML defines a simple layout with four text inputs to display the remaining days, hours, minutes, and seconds. The JavaScript fetches the current date and calculates the difference between the current time and the specified end date. The difference is converted into days, hours, minutes, and seconds, which are then displayed in the respective input fields. The timer updates every second using setInterval, continuously counting down until the target date.",
-    image: "https://i.imghippo.com/files/ARSeL1726747820.png",
+    image: "https://www.imghippo.com/i/pJHy1042t.png",
     tags: ["HTML", "CSS", "JavaScript"],
     github:
       "https://github.com/Bhumi-Maru/Count-Down-App/tree/main/Count-Down%20App",
     webapp: "https://count-down-app-by-maru.netlify.app/",
   },
   {
-    id: 4,
+    id: 3,
     title: "Weather App",
     description:
       "This is a weather forecast website that provides real-time weather updates for any location. Built with HTML, Bootstrap, and JavaScript, the site features a clean and responsive design with a background image for visual appeal. Users can input a city name into a search bar to fetch current weather data using the OpenWeatherMap API. The interface dynamically displays the city name, temperature in Celsius, weather conditions, and the current day. It also includes a weather icon that updates based on the current weather, offering users an engaging and interactive experience.",
-    image: "https://i.imghippo.com/files/q1Gyf1726729433.png",
+    image: "https://i.imghippo.com/files/Tklg7698cA.png",
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/Bhumi-Maru/Weather-App",
     webapp: "https://weather-app-by-maru.netlify.app/",
   },
   {
-    id: 5,
+    id: 4,
     title: "Movie App",
     description:
       "This movie app is designed to showcase popular and searched movies using data from The Movie Database (TMDb) API. Built with HTML, CSS, and JavaScript, it features a search bar that allows users to search for any movie by title. Upon loading, the app fetches and displays the most popular movies dynamically. Each movie is presented in a grid layout with a poster, title, rating, and a brief overview displayed on hover. The app utilizes asynchronous JavaScript to fetch movie data and update the UI in real-time, providing a seamless and responsive movie browsing experience.",
-    image: "https://i.imghippo.com/files/ggczh1726737956.png",
+    image: "https://i.imghippo.com/files/Ce5507Y.png",
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/Bhumi-Maru/Movie-App",
     webapp: "https://movie-app-by-maru.netlify.app/",
   },
   {
-    id: 6,
+    id: 5,
     title: "Alarm-Clock App",
     description:
       "This React-based Alarm Clock app allows users to set a specific time for an alarm to ring, providing both visual and audio alerts. The current time updates every second and is displayed on the interface, while users can input the desired hour, minute, and second for the alarm. When the set time is reached, an animated alarm clock and an alert message appear, and a ringtone plays in a loop. The design is user-friendly, featuring clean and responsive styling, with smooth animations and interactive controls for setting and stopping the alarm.",
-    image: "https://i.imghippo.com/files/9vSHx1726764170.png",
+    image: "https://i.imghippo.com/files/KSd1797Tw.png",
     tags: ["React Js"],
     github: "https://github.com/Bhumi-Maru/Alarm-Clock-App",
     webapp: "https://alarm-clock-by-maru.netlify.app/",
   },
 
   {
-    id: 7,
+    id: 6,
     title: "Todo App",
     date: "",
     description:
       "This React Todo application allows users to add, update, and delete tasks, with state management handled by hooks like useState and useEffect. The app stores tasks in the browser's localStorage, ensuring that the task list persists even after the page is refreshed. Users can type tasks into an input field, add them to the list, and edit or delete tasks directly from the displayed list. The app is styled with CSS, featuring a background image, clean typography, and modern button designs, creating a simple and user-friendly interface.",
-    image: "https://i.imghippo.com/files/OVRNm1726768918.png",
+    image: "https://i.imghippo.com/files/pLUn6708VrA.png",
     tags: ["React Js"],
     github: "https://github.com/Bhumi-Maru/Todo-App",
     webapp: "https://todo-app-by-maru.netlify.app/",
   },
   {
-    id: 8,
+    id: 7,
     title: "Quiz App",
     description:
       "This is a simple quiz application built using React, allowing users to answer multiple-choice questions. It features a smooth user experience with a loading screen that appears for 4 seconds before displaying the quiz interface. The quiz includes ten questions about HTML, CSS, and JavaScript, each with four possible options. Users can select an answer, and the app provides immediate feedback after each question. After completing all the questions, the final score is displayed along with personalized feedback based on the user's performance. The app handles state management using React hooks like useState and useEffect, and it provides a clean and responsive user interface with custom CSS styling.",
-    image: "https://i.imghippo.com/files/WFWO51728455033.png",
+    image: "https://i.imghippo.com/files/kmXW8290xg.png",
     tags: ["React Js"],
     github: "https://github.com/Bhumi-Maru/Quiz-App?tab=readme-ov-file",
     webapp: "https://quiz-app-by-maru.netlify.app/",
   },
 
   {
-    id: 9,
+    id: 8,
     title: "unsplash-clone",
     description:
       "The provided code is a simple React application that serves as an image gallery, allowing users to search for beautiful images from the Unsplash API. Upon loading, the app fetches a default set of nature-themed images and displays them in a visually appealing gallery layout. Users can enter search terms in a text input field and trigger a search either by pressing the Enter key or clicking a button. The app dynamically updates the gallery with images related to the user's query. Each image is displayed in a responsive grid format, with a subtle zoom effect on hover for enhanced interactivity. The application features a clean and modern design, with intuitive navigation elements that adapt for mobile responsiveness. Overall, this gallery provides a seamless user experience for exploring and discovering stunning images.",
-    image: "https://i.imghippo.com/files/4yIEw1728457742.png",
+    image: "https://i.imghippo.com/files/JCQl3916MFw.png",
     tags: ["React Js"],
     github: "https://github.com/Bhumi-Maru/unsplash-clone?tab=readme-ov-file",
     webapp: "https://unsplash-clone-by-maru.netlify.app/",
   },
 
   {
-    id: 10,
-    title: "Snap Snaacks.",
+    id: 9,
+    title: "Snap Snacks.",
     date: "July - 2024",
     description:
       "Snap Snacks is a fast food website designed to offer a seamless online ordering experience. Built with ReactJs, it features a dynamic homepage showcasing popular food items, a robust search and sorting system, and an intuitive shopping cart with real-time updates. Users can securely sign up or log in with validated credentials to manage their orders effortlessly.",
-    image: "https://i.imghippo.com/files/RnCoh1726645734.png",
+    image: "https://i.imghippo.com/files/ze7860gn.png",
     tags: ["React Js"],
     github: "https://github.com/Bhumi-Maru/E-commerce-website-Snap-Snacks.",
     webapp: "https://snap-snacks-by-maru.netlify.app/",
